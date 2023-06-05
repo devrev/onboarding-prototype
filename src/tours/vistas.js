a22523e707156bf259321a240315283328b87315
Nick <escoffier.n@gmail.com>
2023-06-05 11:21:40 -0700

    Update content

--- src/tours/vistas.js
+++ src/tours/vistas.js
@@ -7,6 +7,7 @@ export const vistaTourSteps = [
   {
     type: "point",
     target: "#side_nav__tickets",
+    title: "All your tickets in one place",
     content: "Click here to view all the tickets created by your customers or by you on behalf of your customers",
     disableBeacon: true,
     placement: "right",
@@ -21,7 +22,8 @@ export const vistaTourSteps = [
   {
     type: "point",
     target: "#side_nav__team_activity",
-    content: "Use these default views for your day-to-day support operations",
+    title: "Default views that we have created",
+    content: "We have created default views, also called vistas, to help you get started on DevRev. The Team Activity vista provides an overview of your team's workload, so you can see who is working on what with just one click.",
     disableBeacon: true,
     placement: "right",
   },
@@ -35,8 +37,8 @@ export const vistaTourSteps = [
   {
     type: "point",
     target: "#side_nav__active_ticket_tracker",
-    title: "Ticket Management Made Easy",
-    content: "Stay on top of your work with tickets organised by stages",
+    title: "Stay on top of your work with tickets organised by stages",
+    content: "This view is where your support team operates. Newer tickets appear under \"Queued,\" and support members can assign themselves tickets and move them into \"Work in Progress\" as soon as they begin working on them. Tickets marked as \"Awaiting Customer Response\" do not require immediate action and help you keep your ticket queue organized.",
     disableBeacon: true,
     placement: "right",
   },
@@ -50,7 +52,8 @@ export const vistaTourSteps = [
   {
     type: "point",
     target: "#filter__assigned_to_me",
-    content: "Easily filter tickets assigned to you and start working on them",
+    title: "Filtering, Sorting and Grouping Tickets",
+    content: "Easily filter tickets assigned to you and begin working on them. There are a variety of filtering, sorting, and grouping options available for you to apply based on your personal preference.",
     disableBeacon: true,
     placement: "bottom",
   },
@@ -64,8 +67,8 @@ export const vistaTourSteps = [
   {
     type: "point",
     target: "#side_nav__product_assist_tracker",
-    title: "Seamless Collaboration",
-    content: "Enhance Support and Product collaboration with Tickets organised by stages",
+    title: "Enhance Support and Product collaboration with Tickets organised by stages",
+    content: "The Product Assist Tracker displays tickets that have been escalated to part owners, who can be either PMs or Engineering Managers. These owners can comment on tickets during internal discussions, attach relevant issues, and provide other information to help support members track tickets more effectively. We have included default automations that automatically move ticket stages as linked issues are resolved. This helps your team stay on top of tickets without having to manually follow up.",
     disableBeacon: true,
     placement: "right",
   },
@@ -79,7 +82,8 @@ export const vistaTourSteps = [
   {
     type: "point",
     target: "#action__save_vista",
-    content: "Create your own filtered views, save for future use, and easily share with team members",
+    title: "Share your views with other members",
+    content: "Create your own filtered views that are dynamically updated as new tickets come in. You can save these views for future use, share them with other team members, and collaborate easily.",
     disableBeacon: true,
     placement: "left",
   },
